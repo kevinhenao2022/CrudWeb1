745269100554034651d29c6fee557285dc94be09
505 <505@academicos.cesde.edu.co>
2022-09-13 10:28:03 -0500

    Creamos la conexion a mysql y PDO

---
+++ database.php
@@ -0,0 +1,21 @@
+<?php
+
+$server = "localhost";
+$database = "usuario";
+$user = "root";
+$pass = "";
+
+$conexion = mysqli_connect($server,$user,$pass,$database);
+
+#if (!$conexion) {
+    #echo " *** CONEXION FALLIDA *** ";
+#}
+
+#echo " *** CONEXION EXITOSA :D *** ";
+
+#CONEXION CON PDO
+
+
+
+
+?>
